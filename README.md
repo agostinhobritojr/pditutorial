7de3e2577c6963dac91cdd2bd2c6f3b80f03a31c
agostinhobritojr <agostinhobritojr@gmail.com>
2024-03-16 18:15:19 -0300

    push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pdibook
+# codigos-fonte do tutorial de Introdução ao processamento digital de imagens com OpenCV.
+
+Tutorial disponívem em https://agostinhobritojr.github.io/tutorial/pdi/
---
+++ convolucao/build/CMakeFiles/filtroespacial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filtroespacial.dir/filtroespacial.cpp.o"
+  "CMakeFiles/filtroespacial.dir/filtroespacial.cpp.o.d"
+  "filtroespacial"
+  "filtroespacial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filtroespacial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
